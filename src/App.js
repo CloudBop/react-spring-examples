b0fd892db5fb813e35aeba59e05d607fe6052f57
Cloudbop <boppincolin@gmail.com>
2020-01-10 17:59:14 +0000

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React-Spring Playground</h1>
       </header>
+      <div className="main-container">
+        <h2>Put stuff here</h2>
+      </div>
     </div>
   );
 }
